21d15744bddf28239c8f72443b89bb51c166df00
Ginson Chikafa <gibbschikafa@gmail.com>
2020-05-19 23:13:32 +0200

    First commit

---
+++ hello_world_mpi.c
@@ -0,0 +1,17 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    int rank, size, provided;
+
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    printf("Provided is %d \n", provided);
+    printf("Hello world from rank %d from %d processes\n", rank, size);
+
+    MPI_Finalize();
+
+    return 0;
+}
